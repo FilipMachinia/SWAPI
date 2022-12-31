86a39cb22dc7ae1f866e5224088addbbb6afa4af
FilipMachinia <rubystick@gmail.com>
2022-12-31 12:26:55 +0000

    improved logic to reduce api requests

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Notes:
 - Avatars come from https://comic-cons.xyz/star-wars-avatars/
-- Given more time I would write unit tests and save data in local storage
+- I have tried to use GoodThings colour palette
+- Given more time I would 
+  - save data in local storage
+  - write unit tests
+  - update Angular to v15
+  - refactored hardcoded "if (this.requestReturned === 6)"
--- src/app/characters/character-list/character-list.component.ts
+++ src/app/characters/character-list/character-list.component.ts
@@ -11,9 +11,10 @@ export class CharacterListComponent implements OnInit {
 
   allCharacters: any[] = [];
   charactersToShow = 3;
+  allMovies: any[] = [];
+  requestReturned = 0;
 
-  constructor(private charService: CharacterService, private router: Router) {
-  }
+  constructor(private charService: CharacterService, private router: Router) {}
 
   ngOnInit(): void {
     this.populateList();
@@ -22,19 +23,57 @@ export class CharacterListComponent implements OnInit {
   populateList(): void {
     this.charService.getAllCharacters().subscribe((charRes: any) => {
       this.allCharacters = charRes.results;
-      this.allCharacters.forEach((char, charIndex: number) => {
-        char.films.forEach((film: string, filmIndex: number) => {
-          this.charService.getMovie(film).subscribe((filmRes: any) => {
-            this.allCharacters[charIndex].films[filmIndex] = filmRes.title;
-          })
-        })
+      this.allCharacters.forEach((char) => {
+        this.createMovieIndex(char);
+      });
+
+      this.allMovies.forEach((film: any) => {
+        this.charService.getMovie(film.filmUrl).subscribe((filmRes: any) => {
+
+          this.allMovies.find((movie: any, i: number) => {
+            if (movie.filmUrl === filmRes.url) {
+              this.allMovies[i].title = filmRes.title;
+              this.requestReturned++;
+
+              if (this.requestReturned === 6) {
+                this.updateMovieTitles();
+              }
+              return true; // stop searching
+            }
+          });
+        });
+      });
+    });
+  }
+
+  // set up an array of movies
+  createMovieIndex(char: any): void {
+    char.films.forEach((film: any) => {
+      const movieAlreadyInArray = this.allMovies.find(o => o.filmUrl === film);
+      if (!movieAlreadyInArray) {
+        this.allMovies.push({filmUrl: film, index: film.substring(film.length - 2, film.length - 1)});
+      }
+    })
+  }
+
+  // change movie list from api links to proper titles
+  updateMovieTitles(): void {
+    this.allCharacters.forEach((char, charIndex: number) => {
+      char.films.forEach((film: any, filmIndex: number) => {
+        this.allMovies.find((movie: any, i: number) => {
+          if (movie.filmUrl === film) {
+            this.allCharacters[charIndex].films[filmIndex] = this.allMovies[i];
+          }
+        });
       })
     });
   }
 
+  // go to single character page, send char data
   navigateToSingleChar(name: any) {
     let singleChar = this.allCharacters.find(o => o.name === name);
     const navigationExtras: NavigationExtras = {state: singleChar};
     this.router.navigate(["character", name], navigationExtras);
   }
+
 }
